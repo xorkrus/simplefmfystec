525634011f3e37fc13459144431a6fe6f530b95b
xorkrus <2152104+xorkrus@users.noreply.github.com>
2026-08-31 17:14:53 +0300

    Update config.h

--- include/config.h
+++ include/config.h
@@ -1,7 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Пины
 #define SD_CS       4
 #define SD_MISO     12
 #define SD_MOSI     13
@@ -9,18 +8,14 @@
 #define CS_SENSE    5
 #define LED_PIN     2
 
-// Встроенные SSID/пароль как fallback
 #define FALLBACK_SSID     "xopkland"
 #define FALLBACK_PASSWORD "1234567890987654321"
 
-// Настройки AP
 #define AP_SSID     "sd-card-3dp"
 #define AP_PASSWORD "12345678"
 
-// Таймауты (мс)
 #define WIFI_TIMEOUT 15000
-
-// Размер буфера для чтения файлов
 #define BUFFER_SIZE 4096
+#define HTTP_PORT 80
 
 #endif
